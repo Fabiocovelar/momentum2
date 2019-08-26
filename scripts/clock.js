c25266f3c829c68e72ca13add15d0ac608eb308e
Fabiocovelar <53877834+Fabiocovelar@users.noreply.github.com>
2019-08-26 15:39:39 -0400

    Update clock.js

--- scripts/clock.js
+++ scripts/clock.js
@@ -2,17 +2,16 @@ const clockContainer = document.querySelector(".js-clock");
 const clockTitle = clockContainer.querySelector("h1");
 
 function getTime() {
-    const date = new Date();
-    const hours = date.getHours();
-    const minutes = date.getMinutes();
-    const seconds = date.getSeconds();
+  const date = new Date();
+  const hours = date.getHours();
+  const minutes = date.getMinutes();
+  const seconds = date.getSeconds();
 
-    clockTitle.innerText = ´${hours} : ${minutes} : ${seconds}´ ;
-
+  clockTitle.innerText = `${hours} : ${minutes} : ${seconds}`;
 }
 
 function init() {
-    getTime();
-    setInterval(getTime, 1000);
+  getTime();
+  setInterval(getTime, 1000);
 }
 init();
